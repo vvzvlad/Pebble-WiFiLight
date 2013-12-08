f45f6633016d0e56beed64e2e574ae512bb1a2a7
vvzvlad <vvzvlad@gmail.com>
2013-12-09 01:21:17 +0400

    add test meny layer

--- src/WiFiLight.c
+++ src/WiFiLight.c
@@ -2,59 +2,45 @@
 
 float timer_delay=1; /* Создаем переменную для с временем для таймера */
 Window *window; /* Создаем указатель на окно */
-TextLayer *text_layer;  /* создаем  указатель на текстовый слой */
-enum {
-  QUOTE_KEY_COMMAND = 0x0,
-  QUOTE_KEY_CHANNEL = 0x1,
-};
+TextLayer *text_layer[8];  /* создаем  указатель на текстовый слой */
+static const char* messages[] = {
+"Плафон 1","1",
+"Розетка","2",
+"Плафон 2","3",
+"Лампа","4",
+"5 канал","5",
+"6 канал","6",
+}; 
 
-static void send_msg(int channel, int command) {
-  Tuplet command_tuple = TupletInteger(QUOTE_KEY_COMMAND, command);
-  Tuplet channel_tuple = TupletInteger(QUOTE_KEY_CHANNEL, channel);
-  DictionaryIterator *iter;
-  app_message_outbox_begin(&iter);
+int selected=0;
+Layer *layer;
 
-  if (iter == NULL) {
-    return;
-  }
 
-  dict_write_tuplet(iter, &command_tuple);
-  dict_write_tuplet(iter, &channel_tuple);
-  dict_write_end(iter);
+#define DEF_IP "162.211.231.73"
+#define DEF_PORT "8080"
 
-  app_message_outbox_send();
+static void send_msg(char *channel, char *command, char *ip, char *port) {
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    dict_write_cstring(iter, 0, command);
+    dict_write_cstring(iter, 1, channel);
+    dict_write_cstring(iter, 2, ip);
+    dict_write_cstring(iter, 3, port);
+    app_message_outbox_send();
 }
-
-void config_text_layer(int16_t x,int16_t y,int16_t h,int16_t w, const char *font_key)  /* для исключения дублирования кода, создали функцию, которая занимается инициализаций и настройкой текстового массива*/
-{
-    text_layer = text_layer_create(GRect(x, y, h, w)); /* создаем массив, указываем размер и координаты */
-    text_layer_set_text_color(text_layer, GColorWhite);  /* устанавливаем цвет текста */
-    text_layer_set_background_color(text_layer, GColorClear);  /* устанавливаем цвет фона */
-    text_layer_set_font(text_layer, fonts_get_system_font(font_key)); /* устанавливаем шрифт */
-    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter); /* устанавливаем выравнивание по центру */
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));  /* подключаем текстовый слой к основному в качестве дочернего */
-}
-static void in_received_handler(DictionaryIterator *iter, void *context) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Received!");
-}
-
-static void in_dropped_handler(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Dropped!");
-}
-
-static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Failed to Send!");
-}
-
 
 void up_click(ClickRecognizerRef recognizer, void *context)  /* функция, срабатывающая при клике на кнопки */
 {
-    send_msg(1,1);
+    selected--;
+    layer_mark_dirty(layer);
+    //send_msg("1","on",DEF_IP,DEF_PORT);
 }
 
 void down_click(ClickRecognizerRef recognizer, void *context)  /* функция, срабатывающая при клике на кнопки */
 {
-    send_msg(1,0);
+    //send_msg("1","off",DEF_IP,DEF_PORT);
+    selected++;
+    layer_mark_dirty(layer);
 }
 
 void WindowsClickConfigProvider(void *context)  /* функция, внутри которой должны находиться подписки на кнопки */
@@ -64,22 +50,64 @@ void WindowsClickConfigProvider(void *context)  /* функция, внутри 
     window_single_click_subscribe(BUTTON_ID_DOWN, down_click); 
 }
 
+void layer_update_callback(Layer *me, GContext* ctx) {
+    //graphics_context_set_stroke_color(ctx, GColorBlack);
+    //graphics_context_set_fill_color(ctx, GColorBlack);
+    //graphics_context_set_text_color(ctx, GColorBlack);
+
+    int i;
+    for(i=0;i<6;i++){
+    graphics_draw_line(ctx,GPoint(0,i*29),GPoint(144,i*29));
+    if (i != selected)
+    {
+    text_layer_set_text_color(text_layer[i], GColorBlack);  
+    text_layer_set_background_color(text_layer[i], GColorClear);
+
+        static char buf[11];
+    snprintf(buf, sizeof(buf), "%d", i);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, buf);
+}
+
+    }
+
+
+    text_layer_set_text_color(text_layer[selected], GColorWhite);  
+    text_layer_set_background_color(text_layer[selected], GColorBlack);
+    
+
+}
+
 int main(void)
 {
     window = window_create();  /* Инициализируем окно */
-    window_set_background_color(window, GColorBlack); /* устанавливаем фоновый цвет */
     window_set_fullscreen(window, true); /* включаем полноэкранность */
     window_stack_push(window, true);  /* открываем окно */
-
-    app_message_register_inbox_received(in_received_handler);
-    app_message_register_inbox_dropped(in_dropped_handler);
-    app_message_register_outbox_failed(out_failed_handler);
     app_message_open(64, 64);
 
-    config_text_layer(0, 3, 144, 168, FONT_KEY_GOTHIC_24);
+
+
+    int i;
+    for(i=0;i<6;i++){
+    text_layer[i] = text_layer_create(GRect(0, i*29, 144, i*29+29)); 
+    text_layer_set_text_color(text_layer[i], GColorBlack);  
+    text_layer_set_background_color(text_layer[i], GColorClear);
+    text_layer_set_font(text_layer[i], fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+    text_layer_set_text_alignment(text_layer[i], GTextAlignmentLeft);
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer[i]));
+    text_layer_set_text(text_layer[i], messages[i*2]);
+    }
+    text_layer_set_text_color(text_layer[selected], GColorWhite);  
+    text_layer_set_background_color(text_layer[selected], GColorBlack);
+
+    layer = layer_create(GRect(0, 0, 144, 168));
+    layer_set_update_proc(layer, layer_update_callback);
+    layer_add_child(window_get_root_layer(window), layer);
+
+
+    layer_mark_dirty(layer);
+
+
     window_set_click_config_provider(window, WindowsClickConfigProvider); /* определяем функцию, в которой будут находиться подписки на кнопки */
-    text_layer_set_text(text_layer, "WiFiLight");  /* показываем сообщение при запуске программы */
     app_event_loop();  /* ждем событий */
-    text_layer_destroy(text_layer); /* уничтожаем объекты, освобождаем ресурсы */
     window_destroy(window);  /* уничтожаем объекты, освобождаем ресурсы */
 }
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,19 +1,8 @@
-Pebble.addEventListener("ready",
-function(e) {
-console.log("connect!" + e.ready + e.type);
-});
-
 Pebble.addEventListener("appmessage",
 	function(e) {
-	console.log("message:" + e.payload.channel + e.payload.command);
+	console.log("http://" + e.payload.ip + ":" + e.payload.port + "/api.php?channel=" + e.payload.channel + "&command=" + e.payload.command);
 	var req = new XMLHttpRequest();
-	if (e.payload.command) {
-			req.open('GET', "http://192.168.1.178/api.php?channel=" + e.payload.channel + "&command=on", true);
-	}
-	else
-	{
-			req.open('GET', "http://192.168.1.178/api.php?channel=" + e.payload.channel + "&command=off", true);
-	}
+	req.open('GET', "http://" + e.payload.ip + ":" + e.payload.port + "/api.php?channel=" + e.payload.channel + "&command=" + e.payload.command, true);
 	req.send(null);
 });
 
