7bb7db6fe60301a087f3e28d64ee8feae3bf290e
vvzvlad <vvzvlad@gmail.com>
2013-12-20 20:11:54 +0400

    бля

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,42 +1,9 @@
 Pebble.addEventListener("appmessage",
 	function(e) {
-	console.log("http://146.185.136.209/messages/pebble_app_" + e.payload.UUID + "/next/");
+	console.log("http://" + e.payload.ip + ":" + e.payload.port + "/api.php?channel=" + e.payload.channel + "&command=" + e.payload.command);
 	var req = new XMLHttpRequest();
-	req.open('GET', "http://146.185.136.209/messages/pebble_app_" + e.payload.UUID + "/next/", true);
-
-req.onreadystatechange = function() 
-{
-  if (this.readyState != 4) return;
-var print = this.responseText.replace(/\\033/g, ""); 
-print = print.replace(/\[A/g, "");
-print = print.replace(/\[B/g, "");
-print = print.replace(/\[C/g, "");
-print = print.replace(/\[D/g, "");
-print = print.replace(/\[H/g, "");
-print = print.replace(/\[J/g, "");
-print = print.replace(/\[K/g, "");
-print = print.replace(/\[M/g, "");
-print = print.replace(/\[Y/g, "");
-print = print.replace(/\[X/g, "");
-print = print.replace(/\[R/g, "");
-print = print.replace(/\[V/g, "");
-print = print.replace(/\[W/g, "");
-print = print.replace(/\[b/g, "");
-print = print.replace(/\\r/g, "");
-print = print.replace(/\\n/g, "");
-print = print.replace(/\\t/g, "");
-console.log(print);
-console.log(this.responseText);
-
-          Pebble.sendAppMessage({
-            "print":print,
-        });
-
-  if (this.status != 200) 
-  {
-    return;
-  }
-}
-
+	req.open('GET', "http://" + e.payload.ip + ":" + e.payload.port + "/api.php?channel=" + e.payload.channel + "&command=" + e.payload.command, true);
 	req.send(null);
 });
+
+
